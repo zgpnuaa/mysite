ad4c5fa1daaa226daddb798f3d8f4d6b0a8ab7dd
zhugaoping <zhugaoping@localhost.localdomain>
2019-03-21 22:04:45 +0800

    Initial commit

---
+++ article/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+from slugify import slugify
+
+
+# Create your models here.
+
+
+class ArticleColumn(models.Model):
+    user = models.ForeignKey(User, related_name='article_column', on_delete=models.CASCADE)
+    column = models.CharField(max_length=200)
+    created = models.DateField(auto_now_add=True)
+
+    def _str_(self):
+        return self.column
+
+
+class ArticlePost(models.Model):
+    author = models.ForeignKey(User, related_name="article", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=500)
+    column = models.ForeignKey(ArticleColumn, related_name="article_column", on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-updated",)
+        index_together = (('id', 'slug'),)
+
+    def _str_(self):
+        return self.title
+
+    def save(self, *args, **kargs):
+        self.slug = slugify(self.title)
+        super(ArticlePost, self).save(*args, **kargs)
+
+    def get_absolute_url(self):
+        return reverse("article:article_detail", args=[self.id, self.slug])
+
+    def get_url_path(self):
+        return reverse("article:list_article_detail", args=[self.id, self.slug])
+
+
+
+
+
---
+++ article/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, re_path
+from . import views, list_views
+
+app_name = 'article'
+
+urlpatterns = [
+    path('article-column/', views.article_column, name="article_column"),
+    path('rename-column/', views.rename_article_column, name="rename_article_column"),
+    path('del-column/', views.del_article_column, name="del_article_column"),
+    path('article-post/', views.article_post, name="article_post"),
+    path('del-article/', views.del_article, name="del_article"),
+    path('redit-article/<int:article_id>/', views.redit_article, name="redit_article"),
+    path('article-list/', views.article_list, name="article_list"),
+    #path('article-detail/<int:id>/<slug:slug>/', views.article_detail, name="article_detail"),
+    re_path(r'^article-detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.article_detail, name="article_detail"),
+    path('blog-image-upload/', views.blog_img_upload, name="blog_img_upload", ),
+    path('list-article-titles/', list_views.article_titles, name="article_titles"),
+    path('list-article-detail/<int:id>/<slug:slug>/', list_views.article_detail, name="list_article_detail"),
+    #re_path(r'^list-article-detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', list_views.article_detail, name="list_article_detail"),
+
+]
+
+
+
